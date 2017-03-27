b000d0873662e0361df92ec96ecf7bcc6318668d
Staffan <staffan.sandberg94@gmail.com>
2017-03-27 20:59:08 +0200

    Boards with add card button added in project page. No functionality yet

--- app/project-page/project-page.js
+++ app/project-page/project-page.js
@@ -4,4 +4,7 @@
 angular.module('myApp.projectPage', [])
   .controller('ProjectPageController', function ($scope) {
 
+      $scope.boardTypes = ["To Do","In Progress","Verifying","Done"]
+
+
   });

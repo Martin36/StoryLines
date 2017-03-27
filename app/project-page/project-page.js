c0193aae28903d77e37938c92fc430b795e6e0dc
Staffan <staffan.sandberg94@gmail.com>
2017-03-27 21:11:29 +0200

    Title & Styling
    Added test title in project page. Also changed the styling a bit.

--- app/project-page/project-page.js
+++ app/project-page/project-page.js
@@ -6,5 +6,7 @@ angular.module('myApp.projectPage', [])
 
       $scope.boardTypes = ["To Do","In Progress","Verifying","Done"]
 
+      // TODO: Load project from id in adress field??
+      $scope.projectTitle = "Test Project"
 
   });

c59f95572741ba096ad8ab945432da976087a6e2
Staffan <staffan.sandberg94@gmail.com>
2017-03-28 10:50:13 +0200

    Sidebar updates
    Added more appropriate buttons in the sidebar. Can click on buttons under 'Pages'.

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -2,6 +2,19 @@
  * Created by marti on 2017-03-26.
  */
 angular.module('myApp.sidebar', [])
-  .controller('SidebarController', function ($scope) {
+  .controller('SidebarController', function ($scope, $location) {
+
+    $scope.pages = [{
+      "name": "Planner Hub",
+      "link": "plannerHub"
+    },{
+      "name": "Stories Status",
+      "link": "storiesStatus"
+    }];
+
+    $scope.changeView = function(page) {
+      console.log("Navigating to: "+page);
+      $location.path('/' + page);
+    }
 
 });

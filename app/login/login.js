4429f57ab20492b40c9fd234eaf416bac78825ee
Sandsten <Staffan.Sandberg94@gmail.com>
2017-04-03 15:13:14 +0200

    Google auth firebase test

--- app/login/login.js
+++ app/login/login.js
@@ -6,15 +6,23 @@ angular.module('myApp.login', [])
   .controller('LoginController', function ($scope, $location,
     $firebaseAuth, Model) {
 
-    $scope.login = function () {
-      console.log("Login!");
-      Model.authorize(function () {
-        //Change view to my tasks
-        // $location.path('/myStories');
 
-        // TODO: This will fail to load boards
-        $location.path('/plannerHub');
-      });
+  // Authenticate with google to firebase
+  // TODO: Custom token from trello login maybe??
+  // var auth = $firebaseAuth();
+  // auth.$signInWithPopup("google").then(function(authData) {
+  //   console.log(authData);
+  // }).catch(function(error){
+  //   console.log(error);
+  // });
 
-    };
-  });
+  $scope.login = function () {
+    console.log("Login!");
+    Model.authorize(function () {
+      //Change view to my tasks
+      // $location.path('/myStories');
+      // TODO: This will fail to load boards
+      $location.path('/plannerHub');
+    });
+  };
+});

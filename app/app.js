946e0bc8cf731e30a8c0041143a4871023c4a87c
ioriwitte <iori.witte@runbox.com>
2017-04-05 19:24:24 +0200

    Comments updated.
    I added more explanatory comments on how the route provider works, i.a.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+// Declaration of myApp as single top-level module; myApp will connect sub-modules and apply application-wide
+// configuration settings.
+// Sub-module dependencies are being declared, too (for every myApp feature); for back-end communication and 
+// authentication Node.js and perhaps Firebase will be used; to enable them
+// we need to inject the sub-modules firebase, auth0, angular-jwt, and angular-storage
+// (comments by Jori, April 03, 2017) 
+
 var app = angular.module('myApp', [
   'ngRoute',
   'ngResource',
@@ -13,6 +19,13 @@ var app = angular.module('myApp', [
   'myApp.projectPage',
   'myApp.userScreen'
 ]).
+
+// Passing in $routeProvider, responsible for the configuration of routes
+// Routes are configured by calling $routeProvider.when and passing in a route (URL string), as well as
+// a configuration object for that particular route
+// The route configuration object is responsible for associating a template and a controller 
+// to a particular route (comment by Jori, April 03, 2017)
+
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
@@ -50,5 +63,9 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
                 }
             }
     })
+  
+
+  // In case there is no route match, a fallback is being defined using otherwise method on $routeProvider
+// if a route cannot be matched a redirect to the root of the application occurs (comment by Jori, April 03, 2017)
     .otherwise({redirectTo: '/login'});
 }]);

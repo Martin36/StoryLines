8a4e2e2e8366a86b0e1e9b7870d84ef657e5fab1
Martin <martin.funkquist@gmail.com>
2017-04-08 22:25:50 +0200

    Fixed the dropdown.

--- app/project-page/project-page.js
+++ app/project-page/project-page.js
@@ -5,25 +5,37 @@ angular.module('myApp.projectPage', [])
       // Get 'this' board from model
       $scope.board = Model.getBoard($routeParams.boardId);
       $scope.listsToShow = Model.getListsToShow();
+      $scope.showEdit = false;
 
       $scope.listId = function(listName) {
         return Model.getListId($routeParams.boardId, listName);
-      }
+      };
 
       // TODO: Add card to specific list in this board with API PUSH
       $scope.addCard = function(listName) {
         Model.addNewCard($routeParams.boardId, listName, listName);
-      }
+      };
 
       $scope.editTitle = function(){
         toggleEdit();
-      }
+      };
 
       // Save the new title on trello
       $scope.saveTitle = function(){
         Model.changeBoardName($routeParams.boardId, $scope.board.name);
         toggleEdit();
-      }
+      };
+
+      $scope.showEditBox = function () {
+        $scope.showEdit = true;
+      };
+      //TODO: Save the edits then exit
+      $scope.save = function () {
+        $scope.showEdit = false;
+      };
+      $scope.cancel = function(){
+        $scope.showEdit = false;
+      };
 
       // Toggles the edit mode for project title
       function toggleEdit(){
@@ -31,8 +43,5 @@ angular.module('myApp.projectPage', [])
         $scope.isOpen = !$scope.isOpen;
       }
 
-      //TODO: Add the code for showing the dropdown
-      $scope.showDropdown = function(){
-        $("#")
-      }
+
   });

8591b3d1f4d2627fb86794a09811f8ae6031c24c
Martin Funkquist <martin.funkquist@gmail.com>
2017-04-08 16:16:35 +0200

    Resetting commit

--- app/my-stories/my-stories.js
+++ app/my-stories/my-stories.js
@@ -3,12 +3,15 @@
  */
 angular.module('myApp.myStories', [])
   .controller('MyStoriesController', function ($scope, Model) {
-    //TODO: Wait for data to load in model
-   Model.loadData(function () {
-      $scope.boards = Model.getBoard();
-        $scope.myCards=Model.getUsersCards();
-      //TODO: Figure out why this is needed for it to work
-      $scope.$apply();
-        
-     });
+  if(Model.boardsLoaded()){
+    $scope.boards = Model.getBoards();
+  }else{
+    Model.loadData(function () {
+       $scope.boards = Model.getBoards();
+         //$scope.myCards=Model.getUsersCards();
+       //TODO: Figure out why this is needed for it to work
+       $scope.$apply();
+      });
+  }
+
   });

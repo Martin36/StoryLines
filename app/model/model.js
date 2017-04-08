c813f8e931875973f56db137f5d0259447b0a895
Martin Funkquist <martin.funkquist@gmail.com>
2017-04-08 14:53:46 +0200

    Added new functions to be implemented

--- app/model/model.js
+++ app/model/model.js
@@ -210,6 +210,20 @@ app.factory('Model', function ($resource) {
     return boardsLoaded;
   };
 
+  //Function for the user screen
+
+  //TODO: Implement this function to add a user to the specified board
+  this.addUser = function(boardId, userName){
+    //GET /1/members/[idMember or username]
+
+  }
+
+  //TODO: Make this function remove a user from the board
+  this.removeUser = function(boardId, memberId){
+    //DELETE /1/boards/[board_id]/members/[idMember]
+  }
+
+
   return this;
 
 });

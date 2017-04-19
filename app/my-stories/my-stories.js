bb511b9a42e5647f5ee1a3b8405cb0505a56472b
karishm-jain <karishmajain@outlook.com>
2017-04-19 10:22:13 +0200

    Done button to move the card

--- app/my-stories/my-stories.js
+++ app/my-stories/my-stories.js
@@ -40,5 +40,21 @@ app.controller('MyStoriesController', function ($scope, Model) {
       card.showDropdown = false;
     };
 	$scope.labels=["High Priority","Medium Priority", "Low Priority"];
+	//to come out of the popup.
+	$scope.done=function(){
+		$scope.showEdit=false;
+	};
+	
+	//to move card from one page to another
+	$scope.movecard = function(item, from,to)
+	{
+		console.log('Move card Card: '+Clickedcard+' From:: '+from+' To:: '+to);
+		var idx=from.indexOf(item);
+		if(idx !=-1)
+			{
+				from.splice(idx,1);
+				to.push(card);
+			}
+	};
 }
 );

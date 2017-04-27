bc3cbab228ceb2f5e1863a0fb71a4ddf8754c14d
Martin Funkquist <martin.funkquist@gmail.com>
2017-04-27 16:04:56 +0200

    Fixed layout for home page

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,4 +1,13 @@
 
 app.controller('HomeController', function($scope, $location, Model){
-  
+
+  $scope.changeView = function(page){
+    $location.path('/' + page);
+    setId(page);
+  }
+
+  function setId(id){
+    $scope.currentId = id;
+  }
+
 })
